66eb745d80ebe8216113c406a7d63c34f3497239
Anush Sargsyan <anush.sarg@gmail.com>
2019-08-18 13:18:52 +0400

    Problems 1-4

---
+++ Lecture 1/Practical/Problem3.py
@@ -0,0 +1,9 @@
+Course = "Intro to python"
+print ('Course:',  Course)
+Nickname = "SuperLady"
+print('Nickname:', Nickname)
+grade = 10
+print ('grade:', grade)
+pythonExpert = True
+print('pythonExpert: ', pythonExpert)
+
