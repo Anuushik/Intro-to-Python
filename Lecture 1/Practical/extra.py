26d33fa7f8487b8964c14973069bebcff505d2a7
Anush Sargsyan <anush.sarg@gmail.com>
2019-08-24 16:48:04 +0400

    Extra for Problem 4

---
+++ Lecture 1/Practical/extra.py
@@ -0,0 +1,7 @@
+import math
+AB = input("Add a number: ")
+print ( 'AB = ', AB)
+AC = input("add another number: ")
+print ( 'AC = ', AC)
+BC = math.sqrt(int(AB) ** 2 + int(AC) ** 2)
+print('The hypotenuse of the triangle ABC is:', BC) 
