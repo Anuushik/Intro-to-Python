54ba6b8042198481b5629fefb4df2f84349ae9fd
Anush Sargsyan <anush.sarg@gmail.com>
2019-08-24 23:38:53 +0400

    Homework problem4

---
+++ Lecture 1/Homework/Problem2.py
@@ -0,0 +1,16 @@
+project ="Cake"
+difficulty = 5
+ingredients  = ['flour','butter', 'sugar', 'eggs', 'cocoa powder', 'baking powder']
+print ('sugar' in ingredients)
+print ('butter' in ingredients)
+print ('eggs' or 'margarin' in ingredients)
+print ('eggs' and 'margarin' in ingredients)
+flour = 175
+butter = 175
+sugar = 100  
+eggs = 2 
+cocoa_powder = 1 
+baking_powder = 0.5
+type(flour),type (butter), type(sugar), type(eggs), type(cocoa_powder), type(baking_powder)
+print ('flour -', flour,'\nbutter -', butter, '\nsugar -', sugar, '\neggs -', eggs, 
+       '\ncocoa_powder -', cocoa_powder, '\nbaking_powder -', baking_powder)
