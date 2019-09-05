46e8ed61c9f66ec34206f60f5688252c6ab5e767
Anush Sargsyan <anush.sarg@gmail.com>
2019-09-05 11:00:36 +0400

    H2 P1

--- Lecture_2/Homework/Problem1.py
+++ Lecture_2/Homework/Problem1.py
@@ -1,12 +1,12 @@
 import datetime
 import argparse
 
-tday = datetime.datetime.now()
+tday = datetime.datetime.now( )
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("num_d", help="display number of days", type=int)
-parser.add_argument("num_y", help="display number of years", type=int)
+parser.add_argument("-num_d", help="display number of days", type = int)
+parser.add_argument("-num_y", help="display number of years", type = int)
 
 args = parser.parse_args()
 
@@ -16,7 +16,9 @@ year = datetime.timedelta(days = 365 * args.num_y)
 
 print ("Local current time :", tday)
 
-print("After %s days" % (args.num_d), tday + day )
-print("After %s years" % (args.num_y), tday + year  )
+if args.num_d:
+    print("After %s days" % (args.num_d), tday + day )
+if args.num_y:
+    print("After %s years" % (args.num_y), tday + year  )
 
 
