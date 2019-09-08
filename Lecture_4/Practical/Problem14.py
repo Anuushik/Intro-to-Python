e9a20da90b717ee78f4d59d351c46f2c8b1c47eb
Anush Sargsyan <anush.sarg@gmail.com>
2019-09-08 14:06:44 +0400

    Practical4

--- Lecture_4/Practical/Problem14.py
+++ Lecture_4/Practical/Problem14.py
@@ -1,8 +1,7 @@
 list1 = ["al", "abc", "xyz", "as", "aba","1221"]
 print(list1)
-L2 = [x for x in range(len(list1)) if  x>2 ] #-?
-L3= [ word for word in list1 if word[0]==word[-1]]
-print(L2)
+#L2 = [x for x in range(len(list1)) if   ] #-?
+L3= [ word for word in list1 if word[0]==word[-1] if len(word)>2]
 print(L3)
 
 
