f1379ddab60f24aa881c1eaedab786fc51bb3dd8
Anush Sargsyan <anush.sarg@gmail.com>
2019-09-08 11:49:13 +0400

    Lecture4 P1

---
+++ Lecture_4/Practical/Problem1.py
@@ -0,0 +1,8 @@
+x = int(input("Please enter a number: "))
+y = int(input("another number: "))
+if x > y:
+    print("Th max is: ", x)
+elif x == y:
+    print("they are equal")
+else:
+    print("Max: ", y)    
