b01f98b11e3acf97e6af059c28c906b673f3cf38
Anush Sargsyan <anush.sarg@gmail.com>
2019-10-06 12:44:24 +0400

    Practical8

--- Lecture_5/Practical_5/calc.py
+++ Lecture_5/Practical_5/calc.py
@@ -1,8 +1,15 @@
-def calculate_cube(x)​:
-    return(x^3):
+import pretty_print
 
-x=3    
-print(calculate_cube(x))
+
+def calculate_cube(x):
+    return x**3
 
 def calculate_square(x):
-    return(x^2)
+    return x**2
+
+def main():
+    pretty_print.simple_print(calculate_square(2))
+    pretty_print.pro_print(calculate_cube(4))
+
+if __name__ == '__main__':
+    main()
